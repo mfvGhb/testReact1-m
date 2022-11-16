46d0a695e00e11e37b319f401d0c883b37afc7bb
mfvgml <47579127+mfvgml@users.noreply.github.com>
2022-11-16 00:32:49 +0000

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
 import "./styles.css";
+import App1 from "./App1.js";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see somed dfdfdf magic happen!</h2>
+      <App1 id="bord">чилдрен</App1>
     </div>
   );
 }
---
+++ src/App1.js
@@ -0,0 +1,4 @@
+
+let App1 = (props) =><h3>{props.children}</h3>
+
+export default App1 ;
