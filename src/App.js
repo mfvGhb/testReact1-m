90262ad21c0a5a4c16737742898a19198c08a01c
mfvgml <47579127+mfvgml@users.noreply.github.com>
2022-11-16 23:46:04 +0000

    6

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@
 import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App1 from "./App1.js";
+import App1 , {App2 , App3} from "./App1.js";
 
-let classes={red:"bordRed" , green:"bordGreen"  }
-let objInit={text:' было ' , val:100 , myClass:'bordRed' }
+let app2={className:"bordGreen" } ;
+let app1={text:' было ' , val:100 , myClass:'bordRed' } ;
+let attr3={ className:"bordBlue" , onclick:"f33"} ;
 
 export default function App() {
+
+  let f33=()=>alert("f33") ;
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <App1  {...objInit}> Children </App1>
+      <App1  {...app1}> Children </App1>
+     
+      {App2(app2)}
+
+      <App3 {...attr3}>App3 {attr3.className}</App3>
+      
       <button>Increment+</button><br/><br/>
       <button>Decrement*</button>
-
     </div>
   );
 }
--- src/App1.js
+++ src/App1.js
@@ -1,8 +1,16 @@
 
-let App1 = (props) =>
-<h3 className={props.myClass}>App1: перeдано : {props.children} 
-                                          {props.myClass}
-                                          {props.text}
-                                          {props.val}
-</h3>
+let  App1 = (props) =>
+   <h3 className={props.myClass}>App1: перeдано : {props.children}
+                                                  {props.myClass}
+                                                  {props.text}{props.val}
+   </h3>
+
+let  App2 = (props) =><h3 {...props}> App2: перeдано</h3>
+let  App3 = (props) =><div {...props}>перeдано: {props.children}</div>
+
+
+
 export default App1 ;
+export  {App2} ;
+export  {App3} ;
+
